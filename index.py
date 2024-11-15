51afb2cee6897f482eaaa5cc4418d0c21c80503d
Norman Munge <normanmunge@gmail.com>
2024-11-15 12:43:38 +0300

    Adds user selection parameters eg color, min neighbors and scale effects

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default .xml') #a 
 def hex_to_rbg(hex):
     return tuple(int(hex[i:i+2], 16) for i in (0,2, 4))
     
-def detect_faces(hex_color):
+def detect_faces(hex_color, neighbors = 5, scale = 1.3):
     
     # Initialize the web cam
     cap = cv2.VideoCapture(0)
@@ -38,7 +38,7 @@ def detect_faces(hex_color):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             
         # Detect faces in the image using the pre-trained model
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=scale, minNeighbors=neighbors)
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (color), 2)
@@ -73,16 +73,55 @@ def app():
     """
     st.markdown(html_title_temp, unsafe_allow_html = True)
     
-    # Ask user to select color to draw the rectangle around the detected faces
-    hex_color = st.color_picker('Pick a color to draw around the detected face', '#00FF00')
+    html_subtitle_temp = """
+    <div style="padding:10px; margin-bottom:15px">
+        <h4 style="font-size: 30px"> Let's get started by selecting parameters to adjust our face detection algorithm </h4>
+    </div>
+    """
+    st.markdown(html_subtitle_temp, unsafe_allow_html = True)
     
-    if hex_color:
+    col1, col2, col3 = st.columns(3)
+    
+    # Ask user to select color to draw the rectangle around the detected faces
+    with col1:
+        st.subheader('Color Picker')
+        html_color_temp = """
+        <div style="margin-top:40px"></div>
+        """
+        st.markdown(html_color_temp, unsafe_allow_html = True)
+        hex_color = st.color_picker('Pick a color to draw around the detected face', '#00FF00')
         st.write("The current color is", hex_color)
-        st.write('Press the button below to start detecting faces from your webcam')
+    
+    # minNeighbors
+    with col2:
+        st.subheader('Neighbor')
+        minNeigbors = st.slider("Select the minNeighbors i.e how many neighbors each rectangle should have to retain it.?", min_value=0, max_value=8, value=5, step=1)
+        st.write("The current min neighbor is", minNeigbors)
+    
+    # scale
+    with col3:
+        st.subheader('Scale Picker')
+        html_scale_temp = """
+        <div style="margin-top:20px"></div>
+        """
+        st.markdown(html_scale_temp, unsafe_allow_html = True)
+        scaleFactor = st.slider("Select the scale i.e how much size of the face do you want to be detected?", min_value=0.0, max_value=3.0, value=1.05, step=0.05)
+        st.write("The current scale is", scaleFactor)
+    
+    if hex_color and minNeigbors and scaleFactor:
+        
+        html_spacing_temp = """
+        <div style="margin:0 40px">
+            <hr class="solid">
+        </div>
+        """
+        st.markdown(html_spacing_temp, unsafe_allow_html = True)
+        
+        st.subheader('Press the button below to start detecting faces from your webcam')
         
         # Add a button to start detecting faces
         if st.button('Detect Faces'):
-            image = detect_faces(hex_color)
+            image = detect_faces(hex_color, minNeigbors, scaleFactor)
             
             if st.button("Save Image", on_click=save_image(image)):
                 print('gets here?')
